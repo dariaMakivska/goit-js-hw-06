a72414e88e0f6d0dcbc599bee8c71a54cbbc6612
Daria Makivska <peony.bluet@gmail.com>
2025-02-04 20:38:07 +0200

    Додає код в JS task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,57 @@
+// Напиши скрипт створення і очищення колекції елементів. Користувач вводить кількість
+// елементів в input і натискає кнопку Створити, після чого рендериться колекція.
+// Натисненням на кнопку Очистити, колекція елементів очищається.
+
+// <div id="controls">
+//   <input type="number" min="1" max="100" step="1" />
+//   <button type="button" data-create>Create</button>
+//   <button type="button" data-destroy>Destroy</button>
+// </div>
+// <div id="boxes"></div>
+
+// Створи функцію createBoxes(amount), яка приймає один параметр - число.Функція створює
+// стільки < div >, скільки вказано в amount і додає їх у div#boxes.
+
+// Розміри найпершого <div> - 30px на 30px.
+// Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
+// Всі елементи повинні мати випадковий колір фону у форматі HEX.Використовуй готову
+// функцію getRandomHexColor для отримання кольору.
+// Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі
+// створені елементи.
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const input = document.querySelector("input");
+const btnCreate = document.querySelector("[data-create]");
+const btnDestroy = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+btnCreate.addEventListener("click", onClickCreate);
+btnDestroy.addEventListener("click", destroyBoxes);
+
+function onClickCreate() {
+  const amount = Number(input.value);
+  createBoxes(amount);
+}
+
+function createBoxes(amount) {
+  let size = 30;
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement("div");
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    div.style.marginTop = "10px";
+    size += 10;
+    boxes.appendChild(div);
+  }
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+  input.value = "";
 }
